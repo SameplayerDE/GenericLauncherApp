c7ae976a588b168e642fd829e0700496b523960a
SameplayerDE <asameplayer@gmail.com>
2022-08-26 15:43:30 +0200

    added rest api calls

--- GenericLauncherApp/MainWindow.xaml.cs
+++ GenericLauncherApp/MainWindow.xaml.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GenericLauncherApp
 {
+    public class ToDo
+    {
+        [JsonPropertyName("id")] public int Id { get; set; }
+
+        [JsonPropertyName("name")] public string Name { get; set; }
+
+        [JsonPropertyName("done")] public int Done { get; set; }
+    }
+
+    public class Employee
+    {
+        //https://dummy.restapiexample.com/api/v1/employees
+        [JsonPropertyName("id")] public int Id { get; set; }
+        [JsonPropertyName("employee_name")] public string Name { get; set; }
+        [JsonPropertyName("profile_image")] public string Path { get; set; }
+        [JsonPropertyName("employee_salary")] public int Salary { get; set; }
+        [JsonPropertyName("employee_age")] public int Age { get; set; }
+    }
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +36,44 @@ namespace GenericLauncherApp
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+        
+        public async void SomeMethod()
+        {
+            var result = await Get();
+            var document = JsonDocument.Parse(result);
+            var data = document.RootElement.GetProperty("data");
+            var collection = JsonSerializer.Deserialize<List<Employee>>(data.ToString());
+            DataGrid.ItemsSource = collection;
+        }
+        
+        public static async Task<string> Get()
+        {
+            string message = string.Empty;
+            using (var client = new HttpClient())
+            {
+                //client.BaseAddress = new Uri("http://localhost/Rest/ToDo/todo/");
+                client.BaseAddress = new Uri("https://dummy.restapiexample.com/api/v1/");
+                
+                //var response = await client.GetAsync($"read");
+                var response = await client.GetAsync($"employees");
+                message = await response.Content.ReadAsStringAsync();
+            }
+            return message;
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SomeMethod();
+            /*try
+            {
+                Utils.FetchPosts();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }*/
         }
 
         private void TopBar_Down(object sender, MouseButtonEventArgs e)
@@ -49,10 +100,15 @@ namespace GenericLauncherApp
                 Application.Current.MainWindow!.WindowState = WindowState.Normal;
             }
         }
-        
+
         private void Minimise_OnClick(object sender, RoutedEventArgs e)
         {
             Application.Current.MainWindow!.WindowState = WindowState.Minimized;
+        }
+
+        private void Update_OnClick(object sender, RoutedEventArgs e)
+        {
+            SomeMethod();
         }
     }
 }
